70218eb8f0dd83294a29982446e8cbba1a2f4147
samazevedo <samuel.azevedo@live.com>
2022-01-06 10:55:15 -0500

    add setState to bookable list

--- src/components/Bookables/BookableList.component.jsx
+++ src/components/Bookables/BookableList.component.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { bookables } from '../../static.json'
 
 export default function BookablesList() {
     const group = 'Rooms'
     const bookablesInGroup = bookables.filter((b) => b.group === group)
-    let bookablesIndex = 1
+    const [bookableIndex, setBookableIndex] = useState(1)
 
-    function changeBookable(selectedIndex) {
-        bookablesIndex = selectedIndex
-        console.log(selectedIndex)
-    }
     return (
         <ul classNam='bookables'>
             {bookablesInGroup.map((b, i) => (
                 <li
                     key={b.title}
-                    className={i === bookablesIndex ? 'selected' : null}
+                    className={i === bookableIndex ? 'selected' : null}
                 >
-                    <button className='btn' onClick={() => changeBookable(i)}>
+                    <button className='btn' onClick={() => setBookableIndex(i)}>
                         {b.title}
                     </button>
                 </li>
