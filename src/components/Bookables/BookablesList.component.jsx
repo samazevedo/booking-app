4a08d58917bd432049962334bc1a621522dac968
samazevedo <samuel.azevedo@live.com>
2022-01-14 18:45:00 -0500

    add next button for boobkables items

--- src/components/Bookables/BookablesList.component.jsx
+++ src/components/Bookables/BookablesList.component.jsx
@@ -1,24 +1,47 @@
 import React, { useState } from 'react'
 import { bookables } from '../../static.json'
 import './BookablesList.style.scss'
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa'
 
 export default function BookablesList() {
     const group = 'Rooms'
     const bookablesInGroup = bookables.filter((b) => b.group === group)
     const [bookableIndex, setBookableIndex] = useState(1)
 
+    function nextBookable() {
+        setBookableIndex((i) => (i + 1) % bookablesInGroup.length)
+    }
+    function previousBookable() {
+        setBookableIndex((i) => (i - 1) % bookablesInGroup.length)
+    }
+
     return (
-        <ul className='bookables'>
-            {bookablesInGroup.map((b, i) => (
-                <li
-                    key={b.title}
-                    className={i === bookableIndex ? 'selected' : null}
-                >
-                    <button className='btn' onClick={() => setBookableIndex(i)}>
-                        {b.title}
-                    </button>
-                </li>
-            ))}
-        </ul>
+        <div>
+            <ul className='bookables'>
+                {bookablesInGroup.map((b, i) => (
+                    <li
+                        key={b.title}
+                        className={i === bookableIndex ? 'selected' : null}
+                    >
+                        <button
+                            className='btn'
+                            onClick={() => setBookableIndex(i)}
+                        >
+                            {b.title}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <p>
+                <button className='btn' onClick={previousBookable} autoFocus>
+                    <FaArrowLeft />
+                    <span> Previous</span>
+                </button>
+                <button className='btn' onClick={nextBookable} autoFocus>
+                    <span>Next </span>
+                    <FaArrowRight />
+                </button>
+            </p>
+        </div>
     )
 }
